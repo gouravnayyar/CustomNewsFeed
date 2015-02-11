20b6910aa4aa1111105f22c6544e43fa4feba514
Gourav Nayyar <gnayyar@blr-1st-1-dhcp505.eng.vmware.com>
2015-02-11 15:30:00 +0530

    Initial Commit

---
+++ CustomNewsFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomNewsFeed
+//
+//  Created by Gourav Nayyar on 2/11/15.
+//  Copyright (c) 2015 Gourav Nayyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
